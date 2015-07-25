7e9b86ee3fb93a4f925139f737b7348d03bf4b15
scq000 <scq000@hotmail.com>
2015-07-25 19:36:15 +0800

    import the initial condition

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 var app = require('./src/app/main');
+var tags = [
+  'ITEM000001',
+  'ITEM000001',
+  'ITEM000001',
+  'ITEM000001',
+  'ITEM000001',
+  'ITEM000003-2',
+  'ITEM000005',
+  'ITEM000005',
+  'ITEM000005'
+];
 
-console.log(app.main());
+
+app.main(tags);
