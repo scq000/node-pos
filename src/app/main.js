3a6719760ad788b8b5aa3efc0b5d52bfc4e15c35
scq000 <scq000@hotmail.com>
2015-07-25 19:36:52 +0800

    add main.js

--- src/app/main.js
+++ src/app/main.js
@@ -1,3 +1,23 @@
-exports.main = function printReceipt() {
-  return "The app is running.";
-};
+var POS = require('./model/pos.js');
+var Scanner = require('./model/scanner.js');
+var Cart  = require('./model/cart.js');
+var Utils = require('./model/utils.js');
+
+
+function printReceipt(tags) {
+
+  var cart = new Cart();
+  var scanner = new Scanner();
+  var pos = new POS(cart,scanner);
+  pos.scan(tags);
+
+  pos.discount(Utils.loadPromotions()[0]);
+
+  var receipt = pos.getReceipt();
+
+  console.log(receipt);
+
+}
+
+
+module.exports.main = printReceipt;
