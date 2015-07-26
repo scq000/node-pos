0dcb4849438bf6922f915ee4be8a4b902f1afbcb
scq000 <scq000@hotmail.com>
2015-07-26 20:44:03 +0800

    remove tags in app.js

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
 var app = require('./src/app/main');
-var tags = [
-  'ITEM000001',
-  'ITEM000001',
-  'ITEM000001',
-  'ITEM000001',
-  'ITEM000001',
-  'ITEM000003-2',
-  'ITEM000005',
-  'ITEM000005',
-  'ITEM000005'
-];
-
 
 app.main(tags);
