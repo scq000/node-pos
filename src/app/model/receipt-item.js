8ffb2148ba48feb9eaee83d8e6f373102046571d
scq000 <scq000@hotmail.com>
2015-07-27 13:36:02 +0800

    add receipt-item.js

---
+++ src/app/model/receipt-item.js
@@ -0,0 +1,20 @@
+function ReceiptItem(cartItem,discount) {
+  this.cartItem = cartItem;
+  this.discount = discount;
+}
+
+ReceiptItem.prototype.getSubTotal = function() {
+  return this.cartItem.getSubTotal() - this.discount;
+};
+
+
+ReceiptItem.prototype.getString = function() {
+  var Utils = require('./utils.js');
+
+  return '名称：' + this.cartItem.item.name +
+    '，数量：' + this.cartItem.count + this.cartItem.item.unit +
+    '，单价：' + Utils.formatPrice(this.cartItem.item.price) +
+    '(元)，小计：' + Utils.formatPrice(this.getSubTotal()) + '(元)\n';
+};
+
+module.exports = ReceiptItem;
