7bbca30d5ad05b3acf4ba2ea84c45b42be452be3
scq000 <scq000@hotmail.com>
2015-07-30 12:29:25 +0800

    add tags & change path

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-var app = require('./src/app/main');
+var app = require('./src/main/main');
+
+var tags = [
+  'ITEM000001',
+  'ITEM000001',
+  'ITEM000001',
+  'ITEM000001',
+  'ITEM000001',
+  'ITEM000003-2',
+  'ITEM000005',
+  'ITEM000005',
+  'ITEM000005'
+];
 
 app.main(tags);
